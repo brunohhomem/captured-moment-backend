bc8f8e065fb6132c3762af0b7e806d2facff852a
Bruno H. Homem <brunohhomem@gmail.com>
2025-02-03 18:18:50 -0300

    feat: get all moments by user

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,31 @@
-import fastify from 'fastify'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { routes } from './routes'
+import { authenticateToken } from './middleware/authenticateToken'
+import prismaClient from './prisma'
 
 const app = fastify({ logger: true })
 
 app.register(routes)
+
+app.get(
+  '/get-all-moments',
+  { preHandler: authenticateToken },
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    const { user } = request
+
+    if (!user) {
+      throw new Error('User does not exists!')
+    }
+
+    const registeredMoments = await prismaClient.registeredMoment.findMany({
+      where: {
+        userId: user.userId
+      },
+      orderBy: { isFavorite: 'desc' }
+    })
+
+    return registeredMoments
+  }
+)
 
 export default app
